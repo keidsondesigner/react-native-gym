86ee805fddc6065a578cf88e9bb7a199cd271996
keidson designer <keidsondesigner@gmail.com>
2024-01-31 21:48:04 -0400

    feat: :lipstick: Estilização StatusBar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
-import { Text, View } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
 const [ fontsLoaded ] =  useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#202024' }}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       { fontsLoaded ? <Text>Gym App</Text> : <View /> }
     </View>
   );
