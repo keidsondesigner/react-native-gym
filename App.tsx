e6a73ea9944025f3a5a9e299ae4955e4a7d1cd9c
Keidson Roby <keidsondesigner@gmail.com>
2025-01-04 18:21:33 -0400

    feat: :sparkles: Configuração do GlueStack-Ui

--- App.tsx
+++ App.tsx
@@ -4,14 +4,17 @@ import {
   Roboto_400Regular,
   Roboto_700Bold
  } from '@expo-google-fonts/roboto';
+ import { GluestackUIProvider } from '@gluestack-ui/themed';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      {fontsLoaded ? <Text>Hello world - Gym app!</Text> : <View />}
-    </View>
+    <GluestackUIProvider>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
+          <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+          {fontsLoaded ? <Text>Hello world - Gym app!</Text> : <View />}
+        </View>
+    </GluestackUIProvider>
   );
 }
