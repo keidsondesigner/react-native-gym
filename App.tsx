4905455e393995c27fe9018cb968b5da50a3c355
Keidson Roby <keidsondesigner@gmail.com>
2025-01-04 17:12:12 -0400

    feat: :heavy_plus_sign: Instalado e configurado o expo-font   é o  @expo-google-fonts/roboto

--- App.tsx
+++ App.tsx
@@ -1,9 +1,16 @@
 import { Text, View } from 'react-native';
+import { 
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+ } from '@expo-google-fonts/roboto';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {fontsLoaded ? <Text>Hello world - Gym app!</Text> : <View />}
     </View>
   );
 }
