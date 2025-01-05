cb264e8df1f1dc07e104bdedfec98fabec3a063d
Keidson Roby <keidsondesigner@gmail.com>
2025-01-05 11:46:01 -0400

    feat: :sparkles: Criado Input

---
+++ src/components/input.tsx
@@ -0,0 +1,23 @@
+import { ComponentProps } from "react"
+import { Input as GluestackInput, InputField } from "@gluestack-ui/themed"
+
+// repasando as propriedades com props
+type Props = ComponentProps<typeof InputField>
+
+export function Input({...props}: Props) {
+    return (
+        <GluestackInput
+            w="$full"
+            h="$14"
+            px="$4"
+            bg="$gray700"
+            borderWidth="$0"
+            borderRadius="$md"
+        >
+            <InputField
+                {...props}
+                color="$white"
+            />
+        </GluestackInput>
+    )
+}
