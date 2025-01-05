81d0e540694cd6da5617496d35bdbeed2e51e8d9
Keidson Roby <keidsondesigner@gmail.com>
2025-01-05 10:32:33 -0400

    style: :lipstick: Tema escuro como padrão

--- App.tsx
+++ App.tsx
@@ -4,27 +4,28 @@ import {
   Roboto_400Regular,
   Roboto_700Bold
  } from '@expo-google-fonts/roboto';
- import { Text, GluestackUIProvider } from '@gluestack-ui/themed';
+ import { Text, GluestackUIProvider, Box } from '@gluestack-ui/themed';
  import { config } from './config/gluestack-ui.config';
  
 import { Loading } from '@components/Loading';
+import { SignIn } from '@screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <GluestackUIProvider config={config}>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
+    <GluestackUIProvider config={config} colorMode="dark">
+      <Box flex={1} backgroundColor="$gray700">
         <StatusBar 
-          barStyle="dark-content" 
+          barStyle="light-content" 
           backgroundColor="transparent" 
           translucent 
         />
         {fontsLoaded 
-          ? ( <Text>Hello world - Gym app!</Text> ) 
+          ? ( <SignIn /> ) 
           : ( <Loading /> )
         }
-      </View>
+      </Box>
     </GluestackUIProvider>
   );
 }
