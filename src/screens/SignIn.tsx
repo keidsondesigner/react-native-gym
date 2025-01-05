10f45eabd7591dc0a4f8df1b710574f11a4d38cf
Keidson Roby <keidsondesigner@gmail.com>
2025-01-05 15:13:02 -0400

    fix: :bug: Adicionado ScrollView para evitar elementos um por cima do outro

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { VStack, Image, Center, Text, Heading } from "@gluestack-ui/themed";
+import { VStack, Image, Center, Text, Heading, ScrollView } from "@gluestack-ui/themed";
 
 import BackgroundImage from '@assets/background.png';
 import Logo from '@assets/logo.svg';
@@ -7,49 +7,54 @@ import { Button } from "@components/Button";
 
 export function SignIn() {
     return (
-        <VStack flex={1} bg="$gray700" alignItems="center">
-            <Image 
-                source={BackgroundImage}
-                defaultSource={BackgroundImage}
-                alt="Pessoas treinando"
-                position="absolute"
-                w="$full"
-                h={624}
-            />
+        <ScrollView 
+            contentContainerStyle={{ flexGrow: 1 }}
+            showsVerticalScrollIndicator={false}
+        >
+            <VStack flex={1} bg="$gray700" alignItems="center">
+                <Image 
+                    source={BackgroundImage}
+                    defaultSource={BackgroundImage}
+                    alt="Pessoas treinando"
+                    position="absolute"
+                    w="$full"
+                    h={624}
+                />
 
-            <VStack flex={1} px="$5" w="$full">
-                <Center my="$24">
-                    <Logo />
-                    <Text color="$gray200" fontSize="$sm">
-                        Treine sua mente e o seu corpo
-                    </Text>
-                </Center>
+                <VStack flex={1} px="$5" w="$full">
+                    <Center my="$24">
+                        <Logo />
+                        <Text color="$gray200" fontSize="$sm">
+                            Treine sua mente e o seu corpo
+                        </Text>
+                    </Center>
 
-                <Center gap="$2">
-                    <Heading color="$gray100">
-                        Acesse sua conta
-                    </Heading>
+                    <Center gap="$2">
+                        <Heading color="$gray100">
+                            Acesse sua conta
+                        </Heading>
 
-                    <Input 
-                        placeholder="E-mail" 
-                        keyboardType="email-address" 
-                        autoCapitalize="none"
-                    />
-                    <Input placeholder="Senha" secureTextEntry />
+                        <Input 
+                            placeholder="E-mail" 
+                            keyboardType="email-address" 
+                            autoCapitalize="none"
+                        />
+                        <Input placeholder="Senha" secureTextEntry />
 
-                    <Button title="Acessar"  />
-                </Center>
+                        <Button title="Acessar"  />
+                    </Center>
 
-                <Center flex={1} justifyContent="flex-end" gap="$4" mb="$12">
-                    <Text color="$gray200" fontSize="$sm">
-                        Ainda não tem acesso?
-                    </Text>
+                    <Center flex={1} justifyContent="flex-end" gap="$4" mb="$12" mt="$4">
+                        <Text color="$gray200" fontSize="$sm">
+                            Ainda não tem acesso?
+                        </Text>
 
-                    <Button title="Criar Conta" variant="outline" />
-                </Center>
+                        <Button title="Criar Conta" variant="outline" />
+                    </Center>
+
+                </VStack>
 
             </VStack>
-
-        </VStack>
+        </ScrollView>
     )
 }
