54a35fbca6a3b008286838889b6f27883f6e262b
Keidson Roby <keidsondesigner@gmail.com>
2025-04-06 17:02:16 -0400

    feat: :sparkles: Estilizaçã do Bg e Icones para as telas de navegação no BottomTabs

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { Box } from "@gluestack-ui/themed";
-import { gluestackUIConfig } from "@gluestack-ui/config";
+import { gluestackUIConfig } from '../../config/gluestack-ui.config';
 import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from "./app.routes";
 
