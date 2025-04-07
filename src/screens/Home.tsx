289350dae26099e8d270c7c396e52a5a8e0c2583
Keidson Roby <keidsondesigner@gmail.com>
2025-04-06 22:23:46 -0400

    feat: :sparkles: Implementa lista de GroupTabs componente com FlatList

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,16 +1,38 @@
+import { useState } from "react";
+
 import { GroupTabs } from "@components/GroupTabs";
 import { HomeHeader } from "@components/HomeHeader";
 import { Center, HStack, Text, VStack } from "@gluestack-ui/themed";
+import { FlatList } from "react-native";
 
 export function Home() {
+  const [groupSelected, setgroupSelected] = useState("Costas");
+
+  const [groups, setGroups] = useState([
+    "Costas", "Ombros", "Peito", "Braços", "Pernas", "Abdômen",
+  ]);
+
   return (
     <VStack flex={1}>
       <HomeHeader />
-      <HStack alignItems="center" justifyContent="center" gap={"$4"}>
-        <GroupTabs name="Exercícios"/>
-        <GroupTabs name="Exercícios" isActive/>
-        <GroupTabs name="Exercícios"/>
-      </HStack>
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <GroupTabs
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setgroupSelected(item)}
+            marginRight={16}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 24 }}
+        style={{ marginVertical: 16, maxHeight: 50, minHeight: 50 }}
+      />
+
     </VStack>
   );
 }
