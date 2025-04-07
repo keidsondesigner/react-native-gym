a9080526541a836ad859dc10288a17f07819a010
Keidson Roby <keidsondesigner@gmail.com>
2025-04-06 22:07:53 -0400

    feat: :sparkles: Criado componente GroupTabs

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,16 @@
+import { GroupTabs } from "@components/GroupTabs";
 import { HomeHeader } from "@components/HomeHeader";
-import { Center, Text, VStack } from "@gluestack-ui/themed";
+import { Center, HStack, Text, VStack } from "@gluestack-ui/themed";
 
 export function Home() {
   return (
     <VStack flex={1}>
       <HomeHeader />
+      <HStack alignItems="center" justifyContent="center" gap={"$4"}>
+        <GroupTabs name="Exercícios"/>
+        <GroupTabs name="Exercícios" isActive/>
+        <GroupTabs name="Exercícios"/>
+      </HStack>
     </VStack>
   );
 }
