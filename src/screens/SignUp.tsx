1bae2722ff1904a6f5b8d5d7032507b22f5ea607
Keidson Roby <keidsondesigner@gmail.com>
2025-04-26 23:33:32 -0400

    feat(signup): ✨ Implementado React Hook Form nos Inputs de registro de conta

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -7,14 +7,38 @@ import BackgroundImage from '@assets/background.png';
 import Logo from '@assets/logo.svg';
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
-import { useState } from "react";
+// import { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
 
 export function SignUp() {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [isLoading, setIsLoading] = useState(false);
+    // const [name, setName] = useState('');
+    // const [email, setEmail] = useState('');
+    // const [password, setPassword] = useState('');
+    // const [confirmPassword, setConfirmPassword] = useState('');
+
+    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    // Com React Hook Form, não é necessário criar estados para cada Input
+    // o 'control' é o que controla os Inputs
+    // o 'Controller' é o que controla o estado do Input
+    // o 'name' é o nome do Input
+    // o 'render' é o que renderiza o Input
+    // o 'onChange' é o que atualiza o estado do Input
+    // o 'value' é o valor do Input
+    // o 'defaultValues' é o valor padrão do Input
+    // o 'handleSubmit' é o que envia o formulário
+    // o 'handleSubmit' também passa o 'data' para a função que você passar
+    // o 'data' é o que contém os dados do formulário
+    // o 'formState' é o estado do formulário
+    // o 'errors' é o que contém os erros do formulário
+    const { control, handleSubmit, formState: { errors }} = useForm({
+        defaultValues: {
+            name: '',
+            email: '',
+            password: '',
+            confirmPassword: '',
+        }
+    });
 
     const navigation = useNavigation<AuthNavigatorRoutesProps>();
 
@@ -22,9 +46,12 @@ export function SignUp() {
         navigation.goBack();
     }
 
-    function handleSignUp() {
+    function handleSignUp(data: any) {
         // Criando conta de usuário, usando Input com Estados(useState)
-        console.log({ name, email, password, confirmPassword });
+        // console.log({ name, email, password, confirmPassword });
+
+        // Criando conta de usuário, usando React Hook Form
+        console.log(data);
     }
 
     return (
@@ -55,32 +82,63 @@ export function SignUp() {
                             Crie sua conta
                         </Heading>
 
-                        <Input 
-                            placeholder="Nome"
-                            onChangeText={setName}
+                        <Controller
+                            control={control}
+                            name="name"
+                            render={({ field: { onChange, value }}) => (
+                                <Input
+                                    placeholder="Nome"
+                                    onChangeText={onChange}
+                                    value={value}
+                                />
+                            )}
                         />
 
-                        <Input 
-                            placeholder="E-mail" 
-                            keyboardType="email-address" 
-                            autoCapitalize="none"
-                            onChangeText={setEmail}
+                        <Controller
+                            control={control}
+                            name="email"
+                            render={({ field: { onChange, value }}) => (
+                                <Input
+                                    placeholder="E-mail"
+                                    keyboardType="email-address"
+                                    autoCapitalize="none"
+                                    onChangeText={onChange}
+                                    value={value}
+                                />
+                            )}
                         />
 
-                        <Input
-                            placeholder="Senha"
-                            secureTextEntry
-                            onChangeText={setPassword}
+                        <Controller
+                            control={control}
+                            name="password"
+                            render={({ field: { onChange, value }}) => (
+                                <Input
+                                    placeholder="Senha"
+                                    secureTextEntry
+                                    onChangeText={onChange}
+                                    value={value}
+                                />
+                            )}
                         />
-                        <Input
-                            placeholder="Confirme sua senha"
-                            secureTextEntry
-                            onChangeText={setConfirmPassword}
+
+                        <Controller
+                            control={control}
+                            name="confirmPassword"
+                            render={({ field: { onChange, value }}) => (
+                                <Input
+                                    placeholder="Confirme sua senha"
+                                    secureTextEntry
+                                    onChangeText={onChange}
+                                    value={value}
+                                    onSubmitEditing={handleSubmit(handleSignUp)}
+                                    returnKeyType="send"
+                                />
+                            )}
                         />
 
                         <Button
                             title="Criar e Acessar"
-                            onPress={handleSignUp}
+                            onPress={handleSubmit(handleSignUp)}
                         />
                     </Center>
 
