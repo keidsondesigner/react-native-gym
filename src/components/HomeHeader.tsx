8f51851d337b4f0b991ff60927fd044a1ff86436
Keidson Roby <keidsondesigner@gmail.com>
2025-05-31 00:35:02 -0400

    feat(HomeHeader): ✨Exibindo a foto padrão do usuário até fazer upload da foto

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -3,12 +3,14 @@ import { UserPhoto } from "./UserPhoto";
 import { LogOut } from "lucide-react-native";
 import { useAuth } from "@hooks/useAuth";
 
+import defaultUserImg from "@assets/userPhotoDefault.png";
+
 export function HomeHeader() {
   const { user } = useAuth();
 
   return (
     <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap={"$4"}>
-      <UserPhoto source={{ uri: 'https://github.com/keidsondesigner.png' }} size="sm" />
+      <UserPhoto source={ user.avatar ? { uri: user.avatar } : defaultUserImg } size="sm" />
 
       <VStack flex={1}>
         <Text fontSize="$md" color="$gray100">Olá</Text>
