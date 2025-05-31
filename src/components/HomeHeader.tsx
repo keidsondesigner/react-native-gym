149dff598fbfa29eb21b4ff405764a31c3118d1a
Keidson Roby <keidsondesigner@gmail.com>
2025-05-31 00:30:31 -0400

    feat(HomeHeader): ✨Exibindo nome do usuário

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,15 +1,18 @@
 import { Heading, HStack, Icon, Text, VStack } from "@gluestack-ui/themed";
 import { UserPhoto } from "./UserPhoto";
 import { LogOut } from "lucide-react-native";
+import { useAuth } from "@hooks/useAuth";
 
 export function HomeHeader() {
+  const { user } = useAuth();
+
   return (
     <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap={"$4"}>
       <UserPhoto source={{ uri: 'https://github.com/keidsondesigner.png' }} size="sm" />
 
       <VStack flex={1}>
         <Text fontSize="$md" color="$gray100">Olá</Text>
-        <Heading fontSize="$sm" color="$white">Keidson Roby</Heading>
+        <Heading fontSize="$sm" color="$white">{ user.name }</Heading>
       </VStack>
 
       <Icon as={LogOut} color="$gray200" size="xl" />
